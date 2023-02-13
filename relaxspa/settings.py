ad4b3992dc41ac1f32942d4f5deee130ce583712
KrouTm <caroline-ti@hotmail.com>
2023-02-13 20:52:44 +0000

    removed secret key and set debug

--- relaxspa/settings.py
+++ relaxspa/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_(qgo3((*a57c(h0xawkq^=x-z(a7of5c^4g3p5hr0b2)tzz@&"
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['relaxingspa.herokuapp.com', 'localhost']
 
